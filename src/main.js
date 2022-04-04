e71efb7c6daba36ba1b53cdfa42e63ec4329d979
LAPTOP-8D5THRTF\一枚 <3490790443@qq.com>
2022-04-04 16:46:29 +0800

    第一次提交

---
+++ src/main.js
@@ -0,0 +1,14 @@
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+
+Vue.config.productionTip = false;
+if(process.env.NODE_ENV==='development'){
+  require('./mock')
+}
+new Vue({
+  router,
+  store,
+  render: (h) => h(App),
+}).$mount("#app");
