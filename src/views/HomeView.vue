8e8a8c3f3fb3adba86d02d095af3a7110918ceaa
LAPTOP-8D5THRTF\一枚 <3490790443@qq.com>
2022-04-11 14:45:58 +0800

    3

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,16 +2,16 @@
   <div class="home">
     <!-- <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" /> -->
-    <ol>
+    <!-- <ol>
       <li v-for="(item,index) in arr" :key="index">{{index}}</li>
-    </ol>
+    </ol> -->
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 // import HelloWorld from "@/components/HelloWorld.vue";
-import axios from 'axios';
+// import axios from 'axios';
 export default {
   name: "HomeView",
   data(){
